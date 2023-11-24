0b2512e0a8bcca5bc7d3cf5126965676df99d006
Juan Camilo Gomez Hernandez <juancamilo080253@gmail.com>
2023-11-24 00:15:46 -0500

    Adecuacion grafica, botones de ayuda, y about

--- interfaz.py
+++ interfaz.py
@@ -8,15 +8,16 @@ import funciones as fn
 
 
 def plot_ecuacion(ecuacion, x_vals, puntos=None, ecu_der=None):
-
     plt.title("Grafica de la ecuacion con puntos Dados")
     plt.xlabel("x")
     plt.ylabel("y")
     plt.grid(True)
     plt.axhline(0, color='black', linewidth='1')
     plt.axvline(0, color='black', linewidth='1')
+    plt.plot(x_vals, [fn.evaluar_ecuacion(ecuacion, x) for x in x_vals], label='Ecuación')
 
     canvas.draw()
+    ecuacion_label.config(text=f"Ecuación actual: {ecuacion}")
 
 
 def emular_calculo():
@@ -45,10 +46,40 @@ def emular_calculo():
     resultados_text_widget.delete("1.0", tk.END)
     resultados_text_widget.insert(tk.END, "\n".join(resultados))
 
+def mostrar_ayuda():
+    mensaje_ayuda = "¡Bienvenido a la Calculadora Numérica!\n\n" \
+                    "1. Ingrese la ecuación en el cuadro de texto.\n" \
+                    "2. Ingrese los valores para a, b y el error.\n" \
+                    "3. Seleccione el método numérico.\n" \
+                    "4. Haga clic en 'Emular Cálculo' para ver resultados y gráfica.\n\n" \
+                    "Botón de Ayuda presionado."
+    tk.messagebox.showinfo("Ayuda", mensaje_ayuda)
+
+def mostrar_creditos():
+    mensaje_creditos = "Creado por:\n" \
+                       "- Samuel Celis Lizcano\n" \
+                       "- Juan Pablo Marquez\n" \
+                       "- Yoberson Hernandez\n" \
+                       "- Juan Camilo Gomez Hernandez"
+    tk.messagebox.showinfo("Créditos", mensaje_creditos)
+
+def actualizar_titulo():
+    titulo_label.config(text=ecuacion_entry.get())
 
 ventana = tk.Tk()
 ventana.title("Calculadora Numérica")
 ventana.resizable(False, False)
+
+# Agregar un título centrado
+#Agregar un título centrado y desplazado a la derecha
+titulo_label = ttk.Label(ventana, text="Ecuacion a digitar", font=("Helvetica", 16, "bold"))
+titulo_label.grid(row=0, column=0, columnspan=8, pady=(10, 0), padx=(450, 0)) 
+
+# Configuración de estilos de ttk
+style = ttk.Style()
+style.configure('TButton', padding=5, font=('Helvetica', 12))
+style.configure('TLabel', font=('Helvetica', 12))
+style.configure('TEntry', font=('Helvetica', 12))
 
 # Configuración de la interfaz
 tk.Label(ventana, text="Ecuación:").grid(row=0, column=0, sticky="w")
@@ -76,7 +107,7 @@ metodo_combobox = ttk.Combobox(ventana, textvariable=metodo_var, values=[
 metodo_combobox.grid(row=2, column=1, columnspan=2, sticky="w")
 
 calcular_button = tk.Button(
-    ventana, text="Emular Cálculo", command=emular_calculo)
+    ventana, text="Emular Cálculo", command=lambda: [emular_calculo(), actualizar_titulo()])
 calcular_button.grid(row=2, column=3, columnspan=3)
 
 # Configuración de la columna para mostrar la gráfica, iteraciones y resultados
@@ -91,6 +122,15 @@ toolbar.update()
 
 toolbar_frame.grid(row=6, column=6, columnspan=2, sticky="we")
 
+# Botón de ayuda
+emoji = "\u2753"
+ayuda_button = tk.Button(ventana, text=f"Ayuda {emoji}", command=mostrar_ayuda, bg="blue", fg="white")
+ayuda_button.grid(row=0, column=7, sticky="ne", padx=10, pady=10)
+
+# Botón de créditos con fondo gris oscuro
+creditos_button = tk.Button(ventana, text="Créditos", command=mostrar_creditos, bg="#444", fg="white")
+creditos_button.grid(row=0, column=6, sticky="ne", padx=10, pady=10)
+
 # Frame para las iteraciones
 iteraciones_frame = ttk.LabelFrame(ventana, text="Iteraciones")
 iteraciones_frame.grid(row=4, column=0, columnspan=6, padx=10,
@@ -99,7 +139,11 @@ iteraciones_frame.grid(row=4, column=0, columnspan=6, padx=10,
 # Widget Text para mostrar iteraciones
 iteraciones_text_widget = tk.Text(
     iteraciones_frame, wrap="none", height=10, width=60)
-iteraciones_text_widget.pack(expand=True, fill='both')
+iteraciones_text_widget.pack(side="left",expand=True, fill='both')
+
+# Barra de desplazamiento vertical para el cuadro de texto de iteraciones
+scrollbar = ttk.Scrollbar(iteraciones_frame, orient="vertical", command=iteraciones_text_widget.yview)
+scrollbar.pack(side="right", fill="y")
 
 # Frame para los resultados
 resultados_frame = ttk.LabelFrame(ventana, text="Resultados")
