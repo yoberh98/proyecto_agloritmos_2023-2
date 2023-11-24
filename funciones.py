55449e9648f89e9f7940ce7961a83fa4e2e45fc6
JuanPaMarquez <juan013sanchez@gmail.com>
2023-11-23 22:01:17 -0500

    Se Agrego los ultimos dos metodos, la grafica esta en funcionamiento y se implemento la herramienta de historial

--- funciones.py
+++ funciones.py
@@ -6,9 +6,11 @@ def metodo_biseccion(ecuacion_str, a, b, error):
     iteraciones = []
     resultados = []
 
-    x = symbols('x')
+    x = symbols("x")
     ecuacion = sympify(ecuacion_str)
 
+    ## fa = eval(ecuacion.replace("x", str(a)))
+    ## fb = eval(ecuacion.replace("x", str(b)))
     fa = ecuacion.subs(x, a)
     fb = ecuacion.subs(x, b)
 
@@ -20,16 +22,16 @@ def metodo_biseccion(ecuacion_str, a, b, error):
     while (b - a) / 2 > error:
         iteracion += 1
         c = (a + b) / 2
-        # fc = eval(ecuacion.replace('x', str(c)))
+        ## fc = eval(ecuacion.replace("x", str(c)))
         fc = ecuacion.subs(x, c)
 
         iteraciones.append(
-            f"Iteración {iteracion}: [{a}, {b}], c = {c:.5f}, f(c) = {fc:.5f}")
-
+            f"Iteración {iteracion}: [{a:.5f}, {b:.5f}], c = {c:.5f}, f(c) = {fc:.5f}"
+        )
         if fc == 0:
             break
 
-        if fa * fc < 0:
+        if fa * fc <= 0:
             b = c
             fb = fc
         else:
@@ -37,30 +39,30 @@ def metodo_biseccion(ecuacion_str, a, b, error):
             fa = fc
 
     # Añadir la última iteración y resultados si la condición de error se cumple
-
     resultados.append(
-        f"Error: {error:.5f}\nNúmero de iteraciones: {iteracion}\nRaíz encontrada: {c:.5f}")
+        f"Error: {error:.5f}\nNúmero de iteraciones: {iteracion}\nRaíz encontrada: {c:.5f}"
+    )
 
     return iteraciones, resultados
 
 
 def f_x2(x1, x0, fx1, fx0):
-    x2 = x1 - (fx1*(x1-x0))/(fx1-fx0)
+    x2 = x1 - (fx1 * (x1 - x0)) / (fx1 - fx0)
     return x2
 
 
 def f_errores(x1, x2, fx2):
-    e1 = abs(x2-x1)/abs(x2)
+    e1 = abs(x2 - x1) / abs(x2)
     e2 = abs(fx2)
     return e1, e2
 
 
-def metodo_secante(ecuacion, a, b, error):
+def metodo_secante(ecuacion_str, a, b, error):
     iteraciones = []
     resultados = []
 
-    x = symbols('x')
-    ecuacion = sympify(ecuacion)
+    x = symbols("x")
+    ecuacion = sympify(ecuacion_str)
 
     miEr = 1
     contador = 1
@@ -73,37 +75,104 @@ def metodo_secante(ecuacion, a, b, error):
         fx2 = ecuacion.subs(x, x2)
 
         iteraciones.append(
-            f"Iteración {ite}: [{a}, {b}], xi = {x2:.5f}, f(xi) = {fx2:.5f}")
+            f"Iteración {ite}: [{a:.5f}, {b:.5f}], xi = {x2:.5f}, f(xi) = {fx2:.5f}"
+        )
 
         if contador > 1:
             [e1, e2] = f_errores(b, x2, fx2)
-            if (e1 <= error or e2 <= error):
+            if e1 <= error or e2 <= error:
                 miEr = 2
                 break
         else:
-            contador += 1
+            contador = contador + 1
 
         a = b
         b = x2
         ite += 1
 
     resultados.append(
-        f"Error: {error:.5f}\nNúmero de iteraciones: {ite}\nRaíz encontrada: {x2:.5f}")
+        f"Error: {error:.5f}\nNúmero de iteraciones: {ite}\nRaíz encontrada: {x2:.5f}"
+    )
 
-    # resultados.append("Seleccionaste secante")
+    return iteraciones, resultados
+
+def f_xi(a, b, fa, fb):
+    xi = (a * fb - b * fa) / (fb - fa)
+    return xi
+
+def metodo_falsa_posicion(ecuacion_str, a, b, error):
+    iteraciones = []
+    resultados = []
+
+    x = symbols("x")
+    ecuacion = sympify(ecuacion_str)
+    
+    miErr = 1
+    iteracion = 0
+    xiAnt = 0
+
+    while miErr > error:
+        fa = ecuacion.subs(x, a)
+        fb = ecuacion.subs(x, b)
+
+        xi = f_xi(a, b, fa, fb)
+        fxi = ecuacion.subs(x, xi)
+
+        if (fa * fxi) < 0:
+            b = xi
+        else:
+            a = xi
+
+        if iteracion > 0:
+            miErr = xi - xiAnt
+
+        xiAnt = xi
+        iteracion = iteracion + 1
+
+        iteraciones.append(
+            f"Iteración {iteracion}: [{a:.5f}, {b:.5f}], xi = {xi:.5f}, f(xi) = {fxi:.5f}"
+        )
+
+    resultados.append(
+        f"Error: {error:.5f}\nNúmero de iteraciones: {iteracion}\nRaíz encontrada: {xi:.5f}"
+    )
 
     return iteraciones, resultados
 
 
-def metodo_falsa_posicion(ecuacion, a, b, error):
+def f_xid(x1, fx1, fdx1):
+    xi = x1 - (fx1 / fdx1)
+    return xi
+
+def f_err(x2,x1):
+    e = abs((x2 - x1)/ x2)
+    return e
+
+def metodo_newtom_raphson(ecuacion_str, a, b, error):
     iteraciones = []
     resultados = []
 
-    return iteraciones, resultados
+    x = symbols("x")
+    ecuacion = sympify(ecuacion_str)
+    der_ecuacion = diff(ecuacion, x)
 
+    x1 = a
+    err = 1
 
-def metodo_new(ecuacion, a, b, error):
-    iteraciones = []
-    resultados = []
+    i = 0
+    while err>error:
+        fx1 = ecuacion.subs(x, x1)
+        fdx1 = der_ecuacion.subs(x, x1)
+        x2 = f_xid(x1, fx1, fdx1)
+        err = f_err(x2, x1)
+        x1 = x2
+        i += 1
+        iteraciones.append(
+            f"Iteración {i}: [{a:.5f}, {b:.5f}], xi = {x2:.5f}"
+        )
+    
+    resultados.append(
+        f"Error: {error:.5f}\nNúmero de iteraciones: {i}\nRaíz encontrada: {x2:.5f}"
+    )
 
     return iteraciones, resultados
--- interfaz.py
+++ interfaz.py
@@ -6,10 +6,62 @@ import matplotlib.pyplot as plt
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg, NavigationToolbar2Tk
 import funciones as fn
 
+def emular_calculo():
+    graficar()
+    ecuacion = ecuacion_entry.get()
+    a = float(a_entry.get())
+    b = float(b_entry.get())
+    error = float(error_entry.get())
+    selec = metodo_combobox.get()
+    if selec=="Biseccion":
+        iteraciones, resultados = fn.metodo_biseccion(ecuacion, a, b, error)
+    if selec=="Secante":
+        iteraciones, resultados = fn.metodo_secante(ecuacion, a, b, error)
+    if selec=="Falsa Posicion":
+        iteraciones, resultados = fn.metodo_falsa_posicion(ecuacion, a, b, error)
+    if selec=="Newton-Raphson":
+        iteraciones, resultados = fn.metodo_newtom_raphson(ecuacion, a, b, error)
 
-def plot_ecuacion(ecuacion, x_vals, puntos=None, ecu_der=None):
+    historial.append({
+        'ecuacion': ecuacion,
+        'puntos': [(a), (b)],
+        'error': error,
+        'metodo': selec,
+        'resultados': resultados
+    })
 
-    plt.title("Grafica de la ecuacion con puntos Dados")
+    iteraciones_text_widget.delete("1.0", tk.END)
+    iteraciones_text_widget.insert(tk.END, "\n".join(iteraciones))
+    resultados_text_widget.delete("1.0", tk.END)
+    resultados_text_widget.insert(tk.END, "\n".join(resultados))
+
+    emular_iteraciones()
+    emular_resultados()
+
+
+def emular_iteraciones():
+    iteraciones_frame.update_idletasks()
+
+
+def emular_resultados():
+    resultados_frame.update_idletasks()
+
+
+def plot_ecuacion(ecuacion, x_vals, puntos=None, der_ecuacion=None, rango_x=(-10, 10), paso=0.1):
+    y_vals = [ecuacion.subs('x', val) for val in x_vals]
+
+    plt.cla()
+    plt.plot(x_vals, y_vals)
+
+    if puntos:
+        x_puntos, y_puntos = zip(*puntos)
+        plt.scatter(x_puntos, y_puntos, color='red')
+
+    if der_ecuacion:
+        y_vals = [der_ecuacion.subs('x', val) for val in x_vals]
+        plt.plot(x_vals, y_vals)
+
+    plt.title("Gráfica de la Ecuación con Puntos Dados")
     plt.xlabel("x")
     plt.ylabel("y")
     plt.grid(True)
@@ -18,65 +70,145 @@ def plot_ecuacion(ecuacion, x_vals, puntos=None, ecu_der=None):
 
     canvas.draw()
 
+def graficar():
+    ecuacion_str = ecuacion_entry.get()
+    x = symbols("x")
+    ecuacion = sympify(ecuacion_str)
+    selec = metodo_combobox.get()  
 
-def emular_calculo():
-    ecuacion = ecuacion_entry.get()
-    a = float(a_entry.get())
-    b = float(b_entry.get())
-    error = float(error_entry.get())
-    selec = metodo_combobox.get()
+    punto1 = float(a_entry.get())
+    punto2 = float(b_entry.get())
+    rango1 = punto1 - 5
+    rango2 = punto2 + 5
 
-    if selec == "Biseccion":
-        iteraciones, resultados = fn.metodo_biseccion(ecuacion, a, b, error)
+    puntos = [(punto1, 0), (punto2, 0)]
 
-    if selec == "Secante":
-        iteraciones, resultados = fn.metodo_secante(ecuacion, a, b, error)
-
-    if selec == "Falsa Posicion":
-        iteraciones, resultados = fn.metodo_falsa_posicion(
-            ecuacion, a, b, error)
-
+    rango_x = np.arange(rango1, rango2, 0.1)
+    
     if selec == "Newton-Raphson":
-        iteraciones, resultados = fn.metodo_new(
-            ecuacion, a, b, error)
+        der_ecuacion = diff(ecuacion, x)
+        plot_ecuacion(ecuacion, rango_x, puntos, der_ecuacion)
+    else:
+        plot_ecuacion(ecuacion, rango_x, puntos)
 
-    iteraciones_text_widget.delete("1.0", tk.END)
-    iteraciones_text_widget.insert(tk.END, "\n".join(iteraciones))
-    resultados_text_widget.delete("1.0", tk.END)
-    resultados_text_widget.insert(tk.END, "\n".join(resultados))
+def borrar_historial():
+    historial.clear()
+    with open('historial.txt', 'w') as file:
+        pass  # Simplemente abrir y cerrar el archivo para borrar su contenido
+    historial_text_widget.delete("1.0", tk.END)
+    historial_window.withdraw()
 
+
+def mostrar_historial_emergente():
+    historial_window.deiconify()
+    for entry in historial:
+        historial_text_widget.insert(tk.END, f"Ecuación: {entry['ecuacion']}\n")
+        historial_text_widget.insert(tk.END, f"Puntos: {entry['puntos']}\n")
+        historial_text_widget.insert(tk.END, f"Error: {entry['error']}\n")
+        historial_text_widget.insert(tk.END, f"Método: {entry['metodo']}\n")
+        historial_text_widget.insert(tk.END, "\n".join(entry['resultados']) + "\n\n")
+
+def cargar_historial():
+    try:
+        with open('historial.txt', 'r') as file:
+            # Leer el contenido del archivo y cargar el historial
+            historial.clear()  # Limpiar el historial actual
+            entry = {}
+            for line in file:
+                if line.startswith("Ecuación:"):
+                    entry['ecuacion'] = line.split(":")[1].strip()
+                elif line.startswith("Puntos:"):
+                    entry['puntos'] = eval(line.split(":")[1].strip())
+                elif line.startswith("Error:"):
+                    entry['error'] = float(line.split(":")[1].strip())
+                elif line.startswith("Método:"):
+                    entry['metodo'] = line.split(":")[1].strip()
+                elif line == "\n":
+                    # Fin de la entrada, agregar al historial
+                    if 'iteraciones' in entry and 'resultados' in entry:
+                        historial.append(entry)
+                    entry = {}
+                else:
+                    # Iteraciones o resultados
+                    if 'iteraciones' not in entry:
+                        entry['iteraciones'] = []
+                    if 'resultados' not in entry:
+                        entry['resultados'] = []
+                    entry['iteraciones' if line.startswith('Iteración') else 'resultados'].append(line.strip())
+    except FileNotFoundError:
+        # El archivo no existe, no hay historial para cargar
+        pass
+
+def guardar_historial():
+    with open('historial.txt', 'w') as file:
+        for entry in historial:
+            file.write(f"Ecuación: {entry['ecuacion']}\n")
+            file.write(f"Puntos: {entry['puntos']}\n")
+            file.write(f"Error: {entry['error']}\n")
+            file.write(f"Método: {entry['metodo']}\n")
+            file.write("\n".join(entry['resultados']) + "\n\n")
+
+def cerrar_aplicacion():
+    guardar_historial()
+    ventana.destroy()
+
+def cerrar_historial():
+    historial_text_widget.delete("1.0", tk.END)
+    historial_window.withdraw()
 
 ventana = tk.Tk()
 ventana.title("Calculadora Numérica")
 ventana.resizable(False, False)
+historial = []
+
+fuente_personalizada = ("Arial", 12)
+
+# Definicion de los elementos del Historial
+historial_window = tk.Toplevel(ventana)
+historial_window.title("Historial de Cálculos")
+historial_window.withdraw()
+historial_window.protocol("WM_DELETE_WINDOW", cerrar_historial)
+borrar_button = tk.Button(historial_window, text="Borrar Historial", command=borrar_historial)
+borrar_button.pack(side="top", anchor="nw", pady=10, padx=10)
+
+historial_text_widget = tk.Text(historial_window, wrap="none", height=20, width=80)
+scrollbar = tk.Scrollbar(historial_window, command=historial_text_widget.yview)
+historial_text_widget.configure(yscrollcommand=scrollbar.set)
+
+# Empacar el widget Text y la barra de desplazamiento vertical
+historial_text_widget.pack(side="left", fill="both", expand=True)
+scrollbar.pack(side="right", fill="y")
+cargar_historial()
 
 # Configuración de la interfaz
-tk.Label(ventana, text="Ecuación:").grid(row=0, column=0, sticky="w")
+tk.Label(ventana, text="Ecuación:", font=fuente_personalizada).grid(row=0, column=0, sticky="w")
 ecuacion_entry = tk.Entry(ventana)
 ecuacion_entry.grid(row=0, column=1, columnspan=3, sticky="w")
 
-tk.Label(ventana, text="Punto izquierdo (a):").grid(
-    row=1, column=0, sticky="e")
+tk.Label(ventana, text="Punto izquierdo (a):", font=fuente_personalizada).grid(row=1, column=0, sticky="e")
 a_entry = tk.Entry(ventana, width=10)
 a_entry.grid(row=1, column=1)
 
-tk.Label(ventana, text="Punto derecho (b):").grid(row=1, column=2, sticky="e")
+tk.Label(ventana, text="Punto derecho (b):", font=fuente_personalizada).grid(row=1, column=2, sticky="e")
 b_entry = tk.Entry(ventana, width=10)
 b_entry.grid(row=1, column=3)
 
-tk.Label(ventana, text="Error estimado:").grid(row=1, column=4, sticky="e")
+tk.Label(ventana, text="Error estimado:", font=fuente_personalizada).grid(row=1, column=4, sticky="e")
 error_entry = tk.Entry(ventana, width=10)
 error_entry.grid(row=1, column=5)
 
-tk.Label(ventana, text="Método numérico:").grid(row=2, column=0, sticky="w")
+tk.Label(ventana, text="Método numérico:", font=fuente_personalizada).grid(row=2, column=0, sticky="w")
 metodo_var = tk.StringVar()
-metodo_var.set("Biseccion")
-metodo_combobox = ttk.Combobox(ventana, textvariable=metodo_var, values=[
-                               "Biseccion", "Secante", "Falsa Posicion", "Newton-Raphson"], state="readonly")
+metodo_var.set("Biseccion") 
+metodo_combobox = ttk.Combobox(
+    ventana,
+    textvariable=metodo_var,
+    values=["Biseccion", "Secante", "Falsa Posicion", "Newton-Raphson"],
+    state="readonly", font=fuente_personalizada
+)
 metodo_combobox.grid(row=2, column=1, columnspan=2, sticky="w")
 
-calcular_button = tk.Button(
-    ventana, text="Emular Cálculo", command=emular_calculo)
+calcular_button = tk.Button(ventana, text="Emular Cálculo", command=emular_calculo)
 calcular_button.grid(row=2, column=3, columnspan=3)
 
 # Configuración de la columna para mostrar la gráfica, iteraciones y resultados
@@ -85,30 +217,39 @@ canvas = FigureCanvasTkAgg(fig, master=ventana)
 widget_canvas = canvas.get_tk_widget()
 widget_canvas.grid(row=3, column=6, rowspan=3, sticky="w")
 
+# Crear una barra de herramientas de navegación de matplotlib
 toolbar_frame = tk.Frame(ventana)
 toolbar = NavigationToolbar2Tk(canvas, toolbar_frame)
 toolbar.update()
 
+# Posicionar la barra de herramientas en la interfaz
 toolbar_frame.grid(row=6, column=6, columnspan=2, sticky="we")
 
 # Frame para las iteraciones
 iteraciones_frame = ttk.LabelFrame(ventana, text="Iteraciones")
-iteraciones_frame.grid(row=4, column=0, columnspan=6, padx=10,
-                       pady=10, sticky="w")  # Movido debajo de los inputs
+iteraciones_frame.grid(
+    row=4, column=0, columnspan=6, padx=10, pady=10, sticky="w"
+)  # Movido debajo de los inputs
 
 # Widget Text para mostrar iteraciones
-iteraciones_text_widget = tk.Text(
-    iteraciones_frame, wrap="none", height=10, width=60)
-iteraciones_text_widget.pack(expand=True, fill='both')
+iteraciones_text_widget = tk.Text(iteraciones_frame, wrap="none", height=10, width=70)
+iteraciones_text_widget.pack(expand=True, fill="both")
 
 # Frame para los resultados
 resultados_frame = ttk.LabelFrame(ventana, text="Resultados")
-resultados_frame.grid(row=5, column=0, columnspan=6, padx=10,
-                      pady=10, sticky="w")  # Movido debajo de los inputs
+resultados_frame.grid(
+    row=5, column=0, columnspan=6, padx=10, pady=10, sticky="w"
+)  # Movido debajo de los inputs
 
 # Widget Text para mostrar resultados
-resultados_text_widget = tk.Text(
-    resultados_frame, wrap="none", height=10, width=60)
-resultados_text_widget.pack(expand=True, fill='both')
+resultados_text_widget = tk.Text(resultados_frame, wrap="none", height=10, width=70)
+resultados_text_widget.pack(expand=True, fill="both")
+
+historial_button = tk.Button(ventana, text="Mostrar Historial", command=mostrar_historial_emergente)
+historial_button.grid(row=6, column=0, columnspan=2)
+
+ventana.protocol("WM_DELETE_WINDOW", cerrar_aplicacion)
+
+
 
 ventana.mainloop()
